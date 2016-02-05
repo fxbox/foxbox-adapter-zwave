8ee83b3837058596680672d1ddf91f1c5da29757
Julien Wajsberg <felash@gmail.com>
2016-02-05 19:53:51 +0100

    full handling of options

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ use openzwave::manager;
 
 fn main() {
     manager::options::create("./config/", "", "--SaveConfiguration=true --DumpTriggerLevel=0");
+    manager::options::get().lock().unwrap();
+    manager::options::destroy().unwrap();
     println!("Hello, world!");
 }
