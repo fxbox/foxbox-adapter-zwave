301ef2ab41aab1d9971801cd30a35b0448d57ca7
Julien Wajsberg <felash@gmail.com>
2016-02-08 19:25:22 +0100

    added manager calls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate openzwave;
-use openzwave::options;
+use openzwave::{options, manager};
 
 fn main() {
-    options::create("./config/", "", "--SaveConfiguration=true --DumpTriggerLevel=0");
-    options::get().lock().unwrap();
+    options::create("./config/", "", "--SaveConfiguration=true --DumpTriggerLevel=0").unwrap();
+    options::get().unwrap().lock().unwrap();
+    manager::create().unwrap();
+    manager::get().unwrap();
+    manager::destroy();
     options::destroy().unwrap();
     println!("Hello, world!");
 }
