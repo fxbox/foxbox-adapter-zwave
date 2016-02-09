bf1f0a041938dbb8f2217022a60603426031a7f9
Julien Wajsberg <felash@gmail.com>
2016-02-09 15:55:29 +0100

    testing adding a watcher

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ fn main() {
     options::create("./config/", "", "--SaveConfiguration=true --DumpTriggerLevel=0").unwrap();
     options::get().unwrap().lock().unwrap();
     manager::create().unwrap();
-    manager::get().unwrap();
+    let mut watcher = manager::Watcher::new(
+        |notification: manager::Notification| println!("{}", notification.a)
+    );
+
+    manager::get().unwrap().add_watcher(&mut watcher);
+    manager::get().unwrap().remove_watcher(&mut watcher);
     manager::destroy();
     options::destroy().unwrap();
     println!("Hello, world!");
